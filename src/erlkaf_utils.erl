51d66bbeb7ab354af48fe96808e2fb220bea8cb4
Silviu Caragea <silviu.caragea@woowteam.com>
2017-09-25 12:02:37 +0300

    Fix the way we get the priv directory

--- src/erlkaf_utils.erl
+++ src/erlkaf_utils.erl
@@ -16,12 +16,8 @@
 get_priv_path(File) ->
     case code:priv_dir(erlkaf) of
         {error, bad_name} ->
-            case filelib:is_dir(filename:join(["..", priv])) of
-                true ->
-                    filename:join(["..", priv, File]);
-                false ->
-                    filename:join([priv, File])
-            end;
+            Ebin = filename:dirname(code:which(?MODULE)),
+            filename:join([filename:dirname(Ebin), "priv", File]);
         Dir ->
             filename:join(Dir, File)
     end.
