29a49a562091da1f8e6f3159deceee10d2f6d941
Tony Pitale <tpitale@newrelic.com>
2024-03-11 08:58:46 -0500

    Removing io:format in on_load for erlang 26, temporary

--- src/erlkaf_nif.erl
+++ src/erlkaf_nif.erl
@@ -27,7 +27,6 @@
 
 load_nif() ->
     SoName = erlkaf_utils:get_priv_path(?MODULE),
-    io:format(<<"Loading library: ~p ~n">>, [SoName]),
     ok = erlang:load_nif(SoName, 0).
 
 not_loaded(Line) ->
